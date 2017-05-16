45e6bc121bbf484bff57024daa4a64a6779596e6
bolino <adrien@adriencarpentier.com>
2017-05-16 23:01:33 +0200

    Largeurs et hauteurs max, correction bugs métas avant de poster sur FB

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
 <title>Le générateur de magazines pour public de Michel Drucker</title>
 
-<meta name="Description" content="<?php if(isset($une_string) && $une_string!=''){echo addslashes(strip_tags($une_string));}else{echo "Michel Drucker : le e-magazine";} ?>" />
+<meta name="Description" content="<?php if(isset($une_string) && $une_string!=''){echo strip_tags($une_string);}else{echo "Michel Drucker : le e-magazine";} ?>" />
 <meta name="Generator" content="Notepad++" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">	
 <meta name="theme-color" content="#ffffff">
@@ -11,8 +11,8 @@
 <meta property="og:site_name" content="Michel Drucker e-magazine" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="<?php echo($actual_link);?>" />
-<meta property="og:title" content='<?php if(isset($une_string) && $une_string!=''){echo addslashes(strip_tags($une_string));}else{echo 'Michel Drucker : le e-magazine';} ?>' />
-<meta property="og:description" content='<?php if(isset($article1_string) && $article1_string!=''){echo addslashes(strip_tags($article1_string));}else{echo 'Michel Drucker : le e-magazine';} ?>' />
+<meta property="og:title" content='<?php if(isset($une_string) && $une_string!=''){echo strip_tags($une_string);}else{echo 'Michel Drucker : le e-magazine';} ?>' />
+<meta property="og:description" content="<?php if(isset($article1_string) && $article1_string!=''){echo strip_tags($article1_string);}else{echo 'Michel Drucker : le e-magazine';} ?>" />
 <meta property="og:image" content='<?php if(isset($couv_string) && $couv_string!=''){echo 'http://www.crougnieres.fr/micheldrucker/photos/'.$couv_string;}else{echo 'http://www.crougnieres.fr/micheldrucker/photos/Drucker1.jpg';} ?>' />
 <meta property="fb:app_id" content="1542429205992863" />
 
